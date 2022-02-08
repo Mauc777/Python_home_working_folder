198276970046666ca138c862f428b0a91823e8be
Маис <Kozannostra777@mail.ru>
2022-02-08 19:31:41 +0300

    Решение задачи №3 на Пайтон

---
+++ Task_003_ДЗ_поПитону.py
@@ -0,0 +1,20 @@
+#  Показать числа от -N до N
+import os
+def clear(): return os.system('cls')
+clear()
+
+n = int(input('-->'))
+for i in range(-n-1, n):
+    if (n != 0):
+        print(f'{(i+1)}',)
+
+
+
+
+
+
+
+# for i in Nums:
+        #     if i < len(Nums):
+        #         print(Nums[i] and Nums[i]*-1)
+        
