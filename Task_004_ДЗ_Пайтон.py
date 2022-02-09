c3eadd35fa51e478f9b1fe0465f76b0e17fd7b0a
Маис <Kozannostra777@mail.ru>
2022-02-09 10:24:33 +0300

    Обновленное\Улучшенное2 Решение задачи №4 по Питону

--- Task_004_ДЗ_Пайтон.py
+++ Task_004_ДЗ_Пайтон.py
@@ -1,11 +1,13 @@
 # Показать последнюю цифру трехзначного числа
 
 import os
-clear = lambda: os.system('cls')
+def clear(): return os.system('cls')
 clear()
 
+print('Введите ТРЕХзначное число ...')
+
 a = int(input("-->"))
-if a > 99 and a < 999: 
+if a > 99 and a < 999:
     print(f'последняя цифра числа = {a % 10}')
 else:
-    print("Введено не корректное число")    
+    print("Введено не корректное число")
